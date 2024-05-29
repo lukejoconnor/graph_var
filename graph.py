966f1eb5adbf566ee846f957aaf32daf1b16fe08
Luke Jen O'Connor <loconnor@broadinstitute.org>
2024-05-29 12:41:13 -0400

    Add compute_alleles
    Also add forward_position node attribute

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 from math import inf
 import networkx as nx
 import numpy as np
-from utils import _flip, read_gfa, _node_complement, _edge_complement
+from utils import read_gfa, _node_complement, _edge_complement, _sequence_reversed_complement
 from search_tree import max_weight_dfs_tree
 import os
 
@@ -18,7 +18,7 @@ class PangenomeGraph(nx.DiGraph):
         return '+_terminus', '-_terminus'
 
     @property
-    def biedge_representatives(self) -> list[str]:
+    def sorted_biedge_representatives(self) -> list[str]:
         edges = [edge_with_data for edge_with_data in self.edges(data=True) if edge_with_data[2]['is_representative']]
         return sorted(edges, key=lambda edge: edge[2]['index'])
 
@@ -26,18 +26,22 @@ class PangenomeGraph(nx.DiGraph):
     def biedge_attribute_names(self) -> tuple:
         return 'index', 'weight', 'is_in_tree', 'is_back_edge'
         # TODO add 'is_in_reference_path' such that we can load with edgeinfo file not specifying reference walk index
+    #     TODO add 'position'
 
     @property
     def node_attribute_names(self) -> tuple:
-        return 'direction', 'sequence', 'position'
+        return 'direction', 'sequence', 'position', 'forward_position'
 
     def __init__(self,
-                 directed_graph: nx.classes.digraph.DiGraph = nx.DiGraph(),
-                 reference_tree: nx.classes.digraph.DiGraph = nx.DiGraph(),
+                 directed_graph: nx.classes.digraph.DiGraph = None,
+                 reference_tree: nx.classes.digraph.DiGraph = None,
                  reference_path: list[str] = None,
                  variant_edges: set = None
                  ):
-
+        if directed_graph is None:
+            directed_graph = nx.DiGraph()
+        if reference_tree is None:
+            reference_tree = nx.DiGraph()
         super().__init__(directed_graph)
         self.reference_tree = reference_tree
         self.reference_path = reference_path if reference_path else []
@@ -60,19 +64,21 @@ class PangenomeGraph(nx.DiGraph):
             if not os.path.exists(edgeinfo_file):
                 raise FileNotFoundError(edgeinfo_file)
 
-        nodes, edges, walks, sequences = read_gfa(gfa_file)
+        binodes, biedges, walks, sequences = read_gfa(gfa_file)
 
-        print("Num of Nodes:", len(nodes))
-        print("Num of Edges:", len(edges))
+        print("Num of binodes:", len(binodes))
+        print("Num of biedges:", len(biedges))
 
         # Initialize an instance of the class
         G = cls()
 
-        for node, sequence in zip(nodes, sequences):
-            G.add_binode(node, sequence)
+        for binode, sequence in zip(binodes, sequences):
+            G.add_binode(binode, sequence)
 
-        for edge in edges:
-            G.add_biedge(edge[0] + '_' + edge[2], edge[1] + '_' + edge[3])
+        for biedge in biedges:
+            node1 = biedge[0] + '_' + biedge[2]
+            node2 = biedge[1] + '_' + biedge[3]
+            G.add_biedge(node1, node2)
 
         # Add universal source and sink nodes, also adding terminal edges for each walk
         walk_start_nodes = [walk[0] for walk in walks]
@@ -107,7 +113,7 @@ class PangenomeGraph(nx.DiGraph):
 
         # Add positions
         print("Finish creating tree, start adding position")
-        G.compute_node_positions()
+        G.compute_binode_positions()
 
         if return_walks:
             return G, walks
@@ -179,20 +185,20 @@ class PangenomeGraph(nx.DiGraph):
                 {_node_complement(node) for node in walk_end_nodes if self.nodes[node]['direction'] == -1}
             )
 
-        start_node, end_node = self.termini
-        self.add_binode(start_node)
-        self.add_binode(end_node)
+        plus_terminus, minus_terminus = self.termini
+        self.add_binode(plus_terminus)
+        self.add_binode(minus_terminus)
 
         for source_node in source_nodes:
-            self.add_biedge(start_node + '_+', source_node)
+            self.add_biedge(plus_terminus + '_+', source_node)
 
         for sink_node in sink_nodes:
-            self.add_biedge(sink_node, end_node + '_+')
+            self.add_biedge(sink_node, minus_terminus + '_+')
 
     def compute_reference_tree(self):
 
         # Construct reference tree and ref DAG
-        positive_subgraph = self.subgraph([n for n, attr in self.nodes(data=True) if attr.get('direction') == 1])
+        positive_subgraph = self.subgraph([n for n, direction in self.nodes(data="direction") if direction == 1])
         self.reference_tree, dfs_dag = max_weight_dfs_tree(positive_subgraph,
                                                                       source=self.termini[0] + '_+',
                                                                       reference_path=self.reference_path)
@@ -208,13 +214,65 @@ class PangenomeGraph(nx.DiGraph):
         self.variant_edges = {(u, v) for u, v, data in self.edges(data=True)
                 if data['is_representative'] and not data['is_in_tree']}
 
+    def compute_alleles(self) -> tuple:
+        """
+        Yields the ref and alt allele for variant
+        """
+
+        non_inversion_variants = [(u, v) for u, v in self.variant_edges
+                                  if self.nodes[u]['direction'] == self.nodes[v]['direction']]
+
+
+        positive_direction_variants = [e if self.nodes[e[0]]['direction'] == 1 else _edge_complement(e)
+                                       for e in non_inversion_variants]
+
+        # For each variant edge (u,v), yield the lowest common ancestor of u and v in the tree
+        branch_point_tuples = nx.tree_all_pairs_lowest_common_ancestor(
+            self.reference_tree,
+            root=self.termini[0]+'_+',
+            pairs=positive_direction_variants
+        )
+
+        reversed_tree = nx.reverse(self.reference_tree)
+
+        previous_edge = (None, None)
+        for edge, branch_point in branch_point_tuples:
+            u, v = edge
+
+            # tree_all_pairs_lowest_common_ancestor function yields duplicates for self-pairs
+            if edge == previous_edge:
+                assert u == v, "Found duplicate edge that was not a self-edge"
+                continue
+            previous_edge = edge
+
+            ref_path = nx.shortest_path(reversed_tree, v, branch_point)
+            alt_path = nx.shortest_path(reversed_tree, u, branch_point)
+
+            # ref allele only includes internal nodes on the ref path
+            ref_allele = ''
+            for node in ref_path[-2:0:-1]:
+                ref_allele += self.nodes[node]['sequence']
+
+            # alt allele excludes the branch point, but includes u; if branch point equals u, includes u.
+            if len(alt_path) > 1:
+                alt_path = alt_path[-1:0:-1]
+            alt_allele = ''
+            for node in alt_path:
+                alt_allele += self.nodes[node]['sequence']
+
+            yield (edge, ref_allele, alt_allele)
+
+
+
+
+
     def write_edgeinfo(self, filename: str) -> None:
         with open(filename, 'w') as file:
             # Write the header row
             file.write(','.join(self.biedge_attribute_names) + '\n')
 
             # Sort the edges by 'index' attribute, restricting to representatives
-            sorted_edges = self.biedge_representatives
+            sorted_edges = self.sorted_biedge_representatives
 
             for n, edge in enumerate(sorted_edges):
                 _, _, data = edge
@@ -225,7 +283,7 @@ class PangenomeGraph(nx.DiGraph):
 
     def read_edgeinfo(self, filename: str) -> None:
 
-        sorted_edges = self.biedge_representatives
+        sorted_edges = self.sorted_biedge_representatives
 
         # Read the file
         with open(filename, 'r') as file:
@@ -248,25 +306,25 @@ class PangenomeGraph(nx.DiGraph):
                               if data['is_representative'] and not data['is_in_tree']}
 
         # Define reference tree
-        for edge in self.edges(data='is_in_tree'):
-            if not edge[2]:  # not in tree
+        for u, v, is_in_tree in self.edges(data='is_in_tree'):
+            if not is_in_tree:
                 continue
 
             # Only include forward direction nodes
-            if self.nodes[edge[0]]['direction'] == 1:
-                self.reference_tree.add_edge(*edge[:-1])
+            if self.nodes[u]['direction'] == 1:
+                self.reference_tree.add_edge(u, v)
 
-    def add_binode(self, node: str, seq: str = ''):
+    def add_binode(self, binode: str, seq: str = ''):
         node_data = {key: 0 for key in self.node_attribute_names}
         node_data['sequence'] = seq
         node_data['direction'] = 1
 
-        self.add_node(str(node)+'_+', **node_data)
+        self.add_node(str(binode) + '_+', **node_data)
 
-        node_data['sequence'] = reversed(seq)# TODO map bases to complements
+        node_data['sequence'] = _sequence_reversed_complement(seq)
         node_data['direction'] = -1
 
-        self.add_node(str(node)+'_-', **node_data)
+        self.add_node(str(binode) + '_-', **node_data)
 
     def add_biedge(self, node1: str, node2: str, *, weight: int = 0):
 
@@ -397,19 +455,30 @@ class PangenomeGraph(nx.DiGraph):
 
         return edge_visits
 
-    def compute_node_positions(self):
+    def compute_binode_positions(self):
         # TODO should position be w.r.t. node count or sequence length?
-        # TODO also compute forward position? Needed for anything except defining SNPs?
         for node in self.reference_tree.nodes():
             self.nodes[node]['position'] = -inf
+            self.nodes[node]['forward_position'] = inf
 
         for n, u in enumerate(self.reference_path):
             self.nodes[u]['position'] = n
+            self.nodes[u]['forward_position'] = n
 
-        order = nx.topological_sort(self.reference_tree)
-        next(order)  # skip the root
-        for u in order:
+        order = list(nx.topological_sort(self.reference_tree))
+        for u in order[1:]: # skip the root
             predecessor = next(self.reference_tree.predecessors(u))
             self.nodes[u]['position'] = np.maximum(self.nodes[u]['position'],
                                                self.nodes[predecessor]['position'])
             self.nodes[_node_complement(u)]['position'] = self.nodes[u]['position']
+
+        for u in reversed(order[:-1]):
+            successors = [v for _, v, is_back_edge in self.out_edges(u, data='is_back_edge') if not is_back_edge]
+            if successors:
+                successors_minimum_position = np.min([self.nodes[v]['forward_position'] for v in successors])
+            else: # TODO think about this
+                successors_minimum_position = inf
+
+            self.nodes[u]['forward_position'] = np.minimum(self.nodes[u]['forward_position'],
+                                                            successors_minimum_position)
+            self.nodes[_node_complement(u)]['forward_position'] = self.nodes[u]['forward_position']
