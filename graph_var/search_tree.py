b6bbe3bea3d4bc88caa7ad4d752a8cd2d7a6bd6b
Luke Jen O'Connor <loconnor@broadinstitute.org>
2025-04-14 16:10:47 -0400

    Update search_tree.py
    Ensure that directions are assigned for contigs not connected to linear reference

--- graph_var/search_tree.py
+++ graph_var/search_tree.py
@@ -60,6 +60,8 @@ def assign_node_directions(G: nx.DiGraph,
         # visit current_node
         visited.add(current_node)
         G.nodes[current_node]['direction'] = current_direction
+        visited.add(node_complement(current_node))
+        G.nodes[node_complement(current_node)]['direction'] = -current_direction
         if parent is not None:
             assert G_undirected.has_edge(parent, current_node)
 
@@ -71,8 +73,8 @@ def assign_node_directions(G: nx.DiGraph,
             if neighbor in visited or node_complement(neighbor) in visited:
                 continue
             stack.append((current_node, neighbor, current_direction))
-            stack.append((None, node_complement(neighbor), -current_direction))
 
+    # Initialize the search at the linear reference path so that these nodes all have the same orientation
     for i in range(len(reference_path)):
         if i == 0:
             reference_edge = (None, reference_path[0])
@@ -80,8 +82,18 @@ def assign_node_directions(G: nx.DiGraph,
             reference_edge = (reference_path[i - 1], reference_path[i])
         visit_node(*reference_edge, 1)
 
-    while stack:
-        parent, current_node, current_direction = stack.pop()
-        if current_node not in visited:
-            visit_node(parent, current_node, current_direction)
+    while True:
+        while stack:
+            parent, current_node, current_direction = stack.pop()
+            if current_node not in visited:
+                visit_node(parent, current_node, current_direction)
+        
+        # Ensure that all connected components are visited
+        num_unvisited = G.number_of_nodes() - len(visited)
+        if num_unvisited == 0:
+            break
+        print('Visiting another connected component, with at least {num_unvisited} nodes')
+        unvisited_node = set(G.nodes).difference(visited).pop()
+        visit_node(None, unvisited_node, 1)
+
 
